c937afa44410b5f0b8bddde2284e744103c14c45
Riku <lz354000575@gmail.com>
2024-10-21 08:01:30 +0900

    practice 3 & 4

--- LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bdc675a795d4f74d036455ba52178ab1bce683f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2a1f4330f3edfdf9219de7cbe4eb4b848e10a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
