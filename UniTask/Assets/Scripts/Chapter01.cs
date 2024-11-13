bd5740cc3d0b886bd3440cc2bc2577a3157b971c
Riku <lz354000575@gmail.com>
2024-11-14 07:56:52 +0900

    [UniTask] Chapter01_Part1 2/3

--- UniTask/Assets/Scripts/Chapter01.cs
+++ UniTask/Assets/Scripts/Chapter01.cs
@@ -4,16 +4,34 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Chapter01 : MonoBehaviour
 {
     public TMP_Text TextBoard;
     public Button LoadTextButton;
+    public Button LoadSceneButton;
+    public Slider slider;
 
     void Start()
     {
         LoadTextButton.onClick.AddListener(OnLoadText);
+
+        LoadSceneButton.onClick.AddListener(OnLoadScene);
+    }
+
+    private async void OnLoadScene()
+    {
+        await SceneManager.LoadSceneAsync("Scenes/TargetScene").ToUniTask(
+            (Progress.Create<float>(
+                (p) =>
+                {
+                    Debug.Log("OnLoadScene Progress: " + p);
+                    slider.value = p;
+                }
+            ))
+        );
     }
 
     private async void OnLoadText()
