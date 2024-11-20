429781d23c03d9071d37ace53c546c1834642e6b
RikusyuN <zhoul@nangokrstudios.jp>
2024-11-20 10:37:54 +0900

    [UniTask] Chapter01_Part2 2/2

--- UniTask/Assets/Scripts/Chapter01_2.cs
+++ UniTask/Assets/Scripts/Chapter01_2.cs
@@ -4,28 +4,41 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.LowLevel;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Chapter01_2 : MonoBehaviour
 {
+    public PlayerLoopTiming TestYieldTiming = PlayerLoopTiming.PreUpdate;
     public Button DelayButton;
     public Button DelayFrameButton;
+    public Button YieldButton;
+    public Button NextFrameButton;
+    public Button EndofFrameButton;
+
+    private UniTaskTools uniTaskWaiter;
+
 
     void Start()
     {
         DelayButton.onClick.AddListener(OnClickDelay);
         DelayFrameButton.onClick.AddListener(OnClickDelayFrame);
+        YieldButton.onClick.AddListener(OnClickYield);
+        NextFrameButton.onClick.AddListener(OnClickNextFrame);
+        EndofFrameButton.onClick.AddListener(OnClickEndofFrame);
+        uniTaskWaiter = new UniTaskTools();
     }
 
     private async void OnClickDelay()
-    { 
+    {
         Debug.Log("Delay 1 second");
         Debug.Log(Time.time);
         await UniTask.Delay(TimeSpan.FromSeconds(1));
         Debug.Log(Time.time);
     }
+
     private async void OnClickDelayFrame()
     {
         Debug.Log("Delay 5 frame");
@@ -33,4 +46,47 @@ public class Chapter01_2 : MonoBehaviour
         await UniTask.DelayFrame(5);
         Debug.Log(Time.frameCount);
     }
+    private async void OnClickYield()
+    {
+        Debug.Log("Wait Yield ");
+        Debug.Log("Start " + TestYieldTiming);
+        await uniTaskWaiter.WaitYield(TestYieldTiming);
+        Debug.Log("End " + TestYieldTiming);
+    }
+
+    private async void OnClickNextFrame()
+    {
+        Debug.Log("Wait NextFrame");
+        Debug.Log("Start " + Time.frameCount);
+        await uniTaskWaiter.WaitNextFrame();
+        Debug.Log("End " + Time.frameCount);
+    }
+    private async void OnClickEndofFrame()
+    {
+        Debug.Log("Wait EndofFrame");
+        Debug.Log("Start " + Time.frameCount);
+        await uniTaskWaiter.WaitEndofFrame(this);
+        Debug.Log("End " + Time.frameCount);
+    }
+
+    /// <summary>
+    /// 查看底层系统
+    /// </summary>
+    // private void InjectFunction()
+    // {
+    //     PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();    //获得所有的更新内容
+    //     var subsystems = playerLoop.subSystemList;
+    //     playerLoop.updateDelegate += OnUpdate;
+    //     for (int i = 0; i< subsystems.Length; i++)
+    //     {
+    //         int index = i;
+    //         PlayerLoopSystem.UpdateFunction injectFunction = () =>
+    //         {
+    //             if (!_showUpdateLog) return;
+    //             //输出当前的子系统
+    //             Debug.Log($"subsystem{_showUpdateLog} {subsystems[index]} framecount {Time.frameCount}");
+
+    //         }
+    //     }
+    // }
 }
--- UniTask/Assets/Scripts/UniTaskTools.cs
+++ UniTask/Assets/Scripts/UniTaskTools.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
+using Unity.VisualScripting;
 /// <summary>
 /// 该脚本不继承monobehavriour，即可在非Unity托管中运行
 /// </summary>
@@ -11,5 +12,23 @@ public class UniTaskTools
     {
         ResourceRequest loadOperation = Resources.LoadAsync<T>(path);
         return await loadOperation;
+    }
+
+    public async UniTask<int> WaitYield(PlayerLoopTiming loopTiming)
+    {
+        await UniTask.Yield(loopTiming);
+        return 0;
+    }
+
+    public async UniTask<int> WaitNextFrame()
+    {
+        await UniTask.NextFrame();
+        return Time.frameCount;
+    }
+
+    public async UniTask<int> WaitEndofFrame (MonoBehaviour behaviour)
+    {
+        await UniTask.WaitForEndOfFrame(behaviour);
+        return Time.frameCount;
     }
 }
