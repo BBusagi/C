fe63d4838cdd12575bc1bdfd1c2b671510cb82d4
RikusyuN <zhoul@nangokrstudios.jp>
2024-11-21 09:57:32 +0900

    [UniTask] Chapter01_Part2

--- UniTask/Assets/Scripts/Chapter01_2.cs
+++ UniTask/Assets/Scripts/Chapter01_2.cs
@@ -20,6 +20,10 @@ public class Chapter01_2 : MonoBehaviour
 
     private UniTaskTools uniTaskWaiter;
 
+    private List<PlayerLoopSystem.UpdateFunction> _injectUpdateFunction = new List<PlayerLoopSystem.UpdateFunction>();
+
+    private bool _showUpdateLog = false;
+
 
     void Start()
     {
@@ -29,6 +33,8 @@ public class Chapter01_2 : MonoBehaviour
         NextFrameButton.onClick.AddListener(OnClickNextFrame);
         EndofFrameButton.onClick.AddListener(OnClickEndofFrame);
         uniTaskWaiter = new UniTaskTools();
+
+        InjectFuntion();
     }
 
     private async void OnClickDelay()
@@ -48,45 +54,77 @@ public class Chapter01_2 : MonoBehaviour
     }
     private async void OnClickYield()
     {
+        _showUpdateLog = true;
         Debug.Log("Wait Yield ");
-        Debug.Log("Start " + TestYieldTiming);
+        Debug.Log("[Yield]Start " + TestYieldTiming);
         await uniTaskWaiter.WaitYield(TestYieldTiming);
-        Debug.Log("End " + TestYieldTiming);
+        Debug.Log("[Yield]End " + TestYieldTiming);
+        _showUpdateLog = false;
     }
 
     private async void OnClickNextFrame()
     {
+        _showUpdateLog = true;
         Debug.Log("Wait NextFrame");
-        Debug.Log("Start " + Time.frameCount);
+        Debug.Log("[NextFrame]Start " + Time.frameCount);
         await uniTaskWaiter.WaitNextFrame();
-        Debug.Log("End " + Time.frameCount);
+        Debug.Log("[NextFrame]End " + Time.frameCount);
+        _showUpdateLog = false;
     }
     private async void OnClickEndofFrame()
     {
+        _showUpdateLog = true;
         Debug.Log("Wait EndofFrame");
-        Debug.Log("Start " + Time.frameCount);
+        Debug.Log("[EndofFrame]Start " + Time.frameCount);
         await uniTaskWaiter.WaitEndofFrame(this);
-        Debug.Log("End " + Time.frameCount);
+        Debug.Log("[EndofFrame]End " + Time.frameCount);
+        _showUpdateLog = false;
     }
 
     /// <summary>
     /// 查看底层系统
     /// </summary>
-    // private void InjectFunction()
-    // {
-    //     PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();    //获得所有的更新内容
-    //     var subsystems = playerLoop.subSystemList;
-    //     playerLoop.updateDelegate += OnUpdate;
-    //     for (int i = 0; i< subsystems.Length; i++)
-    //     {
-    //         int index = i;
-    //         PlayerLoopSystem.UpdateFunction injectFunction = () =>
-    //         {
-    //             if (!_showUpdateLog) return;
-    //             //输出当前的子系统
-    //             Debug.Log($"subsystem{_showUpdateLog} {subsystems[index]} framecount {Time.frameCount}");
+    private void InjectFuntion()
+    {
+        PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();    //获得所有的更新内容
+        var subsystems = playerLoop.subSystemList;
+        playerLoop.updateDelegate += OnUpdate;
+        for (int i = 0; i< subsystems.Length; i++)
+        {
+            int index = i;
+            PlayerLoopSystem.UpdateFunction injectFunction = () =>
+            {
+                if (!_showUpdateLog) return;
+                //输出当前的子系统
+                Debug.Log($"{_showUpdateLog} Subsystem:[{subsystems[index]}] / FC:{Time.frameCount}");
+            };
+            _injectUpdateFunction.Add(injectFunction);
+            subsystems[i].updateDelegate += injectFunction;
+        }
+        PlayerLoop.SetPlayerLoop(playerLoop);
+    }
 
-    //         }
-    //     }
-    // }
+    private void UnInjectFunction()
+    {
+        PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+        playerLoop.updateDelegate -= OnUpdate;
+        var subsystem = playerLoop.subSystemList;
+        for(int i = 0; i < subsystem.Length; i++)
+        {
+            subsystem[i].updateDelegate -= _injectUpdateFunction[i];
+        }
+
+        PlayerLoop.SetPlayerLoop(playerLoop);
+        _injectUpdateFunction.Clear();
+    }
+
+    private void OnUpdate()
+    {
+        Debug.Log("Current Framecount " + Time.frameCount);
+    }
+
+    private void OnDestory()
+    {
+        UnInjectFunction();
+    }
 }
